36d5fe0a61260a44040f704f183759668ece9894
Abu Taher Molla <57549941+AbuTaherMolla24@users.noreply.github.com>
2020-08-11 19:46:55 +0600

    end

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+let pakhi;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+function add(num1, num2){
+    console(num1);
+}
+
+
+const result = add(22,33);
+console.log(result);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [3,3,3,];
+console.log(ages[11]);
+
