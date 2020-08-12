1ea665d71ea844b806c8b983a19983d58301554e
Abu Taher Molla <57549941+AbuTaherMolla24@users.noreply.github.com>
2020-08-12 17:43:38 +0600

    end

---
+++ scope.js
@@ -0,0 +1,15 @@
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    //console.log(bonus);
+    if(result >9){
+        var mood = 'happy';
+        mood = 'funky';
+        // console.log(mood);
+    }
+    
+    let day = 'friday';
+    console.log(day);
+    return result;
+}
